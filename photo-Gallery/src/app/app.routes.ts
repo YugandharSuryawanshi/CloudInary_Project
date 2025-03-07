f12cbb5c1468dda73d94623d8a3fcfb400a00941
Yugandhar Suryawanshi <yugandharsuryawanshi0@gmail.com>
2025-03-07 18:04:27 +0530

    add new Login Component

--- photo-Gallery/src/app/app.routes.ts
+++ photo-Gallery/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DbImagesComponent } from './db-images/db-images.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'db_images', component: DbImagesComponent},
+    { path: 'login', component: LoginComponent},
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
---
+++ photo-Gallery/src/app/login/login.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule, FormsModule, ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  isLoginForm = true;
+  showLoginPassword = false;
+  showRegisterPassword = false;
+
+  loginData = {
+    email: '',
+    password: ''
+  };
+
+  registerData = {
+    name: '',
+    email: '',
+    password: ''
+  };
+
+  onLoginSubmit(form: any) {
+    if (form.valid) {
+      console.log('Login Data:', this.loginData);
+      // Add your login logic here
+    }
+  }
+
+  onRegisterSubmit(form: any) {
+    if (form.valid) {
+      console.log('Registration Data:', this.registerData);
+      // Add your registration logic here
+    }
+  }
+  
+
+}
