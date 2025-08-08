ebcbba1eef6a4b990a6fbaf3b642e8ccde933e97
Yugandhar Suryawanshi <yugandharsuryawanshi0@gmail.com>
2025-08-08 11:30:37 +0530

    Doing changes in server file

--- photo-Gallery/src/server.ts
+++ photo-Gallery/src/server.ts
@@ -37,7 +37,8 @@ app.get('**', (req, res, next) => {
 
 /**
  * Start the server if this module is the main entry point.
- * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
+ * The server listens on the port defined by the `PORT` environment variable,
+ * or defaults to 4000.
  */
 
 if (isMainModule(import.meta.url)) {
