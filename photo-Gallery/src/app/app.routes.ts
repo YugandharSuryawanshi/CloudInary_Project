64a90ff5810cefeff50334e93d8f47b2203bc94b
Yugandhar Suryawanshi <yugandharsuryawanshi0@gmail.com>
2025-08-27 21:56:51 +0530

    Route Update

--- photo-Gallery/src/app/app.routes.ts
+++ photo-Gallery/src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { DbImagesComponent } from './db-images/db-images.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent},
     { path: 'db_images', component: DbImagesComponent},
     { path: 'login', component: LoginComponent},
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
